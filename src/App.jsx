d816eaff7b6afabc9f6ca5b6c825d024a6a3c69e
Nikhil <nikhilsharma52197@gmail.com>
2024-02-27 01:34:48 +0530

    tryin gdifferent dash boards

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ import Enter from "./Enter.jsx";
 import ProtectedRoute from "./ProtectedRoute.jsx";
 import Roger from "./Roger.jsx";
 import About from "./About.jsx";
+import DashBoard from "./DashBoard.jsx";
+import MiniVarient from "./dash/MiniVarient.jsx";
+import Setting from "./dash/Setting.jsx";
+import { Home } from "@mui/icons-material";
+
 // import Read from "./Read.jsx";
 const Read = lazy(()=> import( "./Read.jsx"))
 
@@ -14,12 +19,23 @@ const App = () => {
   return (
     <>
   <Routes>
+    {/*  */}
+    <Route path="/" element={<MiniVarient/>}>
+    <Route path="/setting" element={<Setting/>}/>
+    <Route path="/home" element={<Home/>}/>
+    </Route>
+    
+
+
+    {/*  */}
+  <Route   path="/dash" element={<DashBoard/>}/>
   <Route   element={<ProtectedRoute/>}>
+  
   <Route path="/enter"  element={<Enter/>}/>
   <Route path="/enter/next"  element={<Roger/>}/>
   </Route>
  <Route path="/about" element={<About/>}/>
-  <Route path="/" element={<Roger/>}/>
+  <Route path="/rr" element={<Roger/>}/>
   <Route path="/r" element={<Read/>}/>
 
   <Route path="*" element={<div>404 not found</div>} />
